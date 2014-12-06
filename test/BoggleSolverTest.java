b202db2154a5250708612f4371ee104f23b70029
Valdermeyder <vguzar@rambler.ru>
2014-12-07 00:53:05 +0200

    Add first test

---
+++ test/BoggleSolverTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BoggleSolverTest {
+    private BoggleSolver solver;
+    private BoggleBoard board;
+
+    private void readFiles(String dictionaryFileName, String boardFileName) {
+        In in = new In(dictionaryFileName);
+        String[] dictionary = in.readAllStrings();
+        solver = new BoggleSolver(dictionary);
+        board = new BoggleBoard(boardFileName);
+    }
+
+    private int calculateBoggleSolverScore() {
+        int score = 0;
+        for (String word : solver.getAllValidWords(board)) {
+            score += solver.scoreOf(word);
+        }
+        return score;
+    }
+
+    @Test
+    public void testBoard4x4WithDictionaryAlgs4() {
+        readFiles("dictionary-algs4.txt", "board4x4.txt");
+        assertEquals("Score for Boggle Solver is wrong", 33, calculateBoggleSolverScore());
+    }
+}
